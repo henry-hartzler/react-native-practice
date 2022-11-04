f0bc1fe87fe3f2a4cde18303d1e2bd97dab78762
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-04 14:27:06 -0600

    View Mosh video

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableHighlight, Image, Button } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text pressed")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello React Native</Text>
+      <TouchableHighlight onPress={() => console.log('image tapped')}>
+        <Image 
+          source={{ 
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"}} />
+      </TouchableHighlight>
       <StatusBar style="auto" />
-    </View>
+      {/* <Button color="orange" title='Click Me' onPress={() => console.log("button tapped")}/> */}
+    </SafeAreaView>
   );
 }
 
