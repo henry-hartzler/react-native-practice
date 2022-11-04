5e86c258e7181419a39acd9ae6b0351e1ca58050
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-04 14:27:38 -0600

    Button

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -9,15 +9,7 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>Hello React Native</Text>
-      <TouchableHighlight onPress={() => console.log('image tapped')}>
-        <Image 
-          source={{ 
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300"}} />
-      </TouchableHighlight>
-      <StatusBar style="auto" />
-      {/* <Button color="orange" title='Click Me' onPress={() => console.log("button tapped")}/> */}
+      <Button color="orange" title='Click Me' onPress={() => console.log("button tapped")}/>
     </SafeAreaView>
   );
 }
