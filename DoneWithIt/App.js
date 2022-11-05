ed5dd48599aceef34babde2d87bcc784b6230b69
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-04 20:23:20 -0600

    conditional rendering for portrait and ls

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -6,21 +6,23 @@ import {
   View,
   Dimensions
 } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
   //helpful to check the dimensions of that phone - but doesnt respond to orientation changes
-  console.log(Dimensions.get("screen"))
+  //destructure landscape from useDeviceOrientation
+  const { landscape } = useDeviceOrientation();
 
   return (
     //style on the right overwrites those to the left
     //SafeAreaView may only work on iOS
-    <SafeAreaView style={[styles.container, containerStyle]}>
+    <SafeAreaView style={styles.container}>
      <View style={{
       backgroundColor: 'dodgerblue',
       //density-independent pixels
       //physical pixels = DIPs x Scale Factor of device
       width: '100%',
-      height: '30%'
+      height: landscape ? '100%' : '30%'
      }}
      >
 
@@ -28,8 +30,6 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-const containerStyle = { backgroundColor: "orange" };
 
 //ctrl + space gives acceptable options list
 
