cbfa51bbb869168a218770254fd2cfc3d7da6412
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-04 20:30:29 -0600

    flex basics

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,33 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { 
-  StyleSheet,  
-  SafeAreaView,
-  Platform,
-  View,
-  Dimensions
-} from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import { View } from 'react-native';
 
 export default function App() {
-  //helpful to check the dimensions of that phone - but doesnt respond to orientation changes
-  //destructure landscape from useDeviceOrientation
-  const { landscape } = useDeviceOrientation();
-
   return (
-    //style on the right overwrites those to the left
-    //SafeAreaView may only work on iOS
-    <SafeAreaView style={styles.container}>
-     <View style={{
-      backgroundColor: 'dodgerblue',
-      //density-independent pixels
-      //physical pixels = DIPs x Scale Factor of device
-      width: '100%',
-      height: landscape ? '100%' : '30%'
-     }}
-     >
-
-     </View>
-    </SafeAreaView>
+    <View style={{
+      backgroundColor: "#fff",
+      flex: 1
+    }}
+    >
+      <View style={{
+        backgroundColor: "dodgerblue",
+        flex: 2
+      }}
+      />
+      <View style={{
+        backgroundColor: "gold",
+        flex: 1
+      }}
+      />
+      <View style={{
+        backgroundColor: "tomato",
+        flex: 1
+      }}
+      />
+    </View>
   );
 }
 
@@ -35,12 +31,12 @@ export default function App() {
 
 //conventionally, styles are written below their component
 //video paused at 1:09:59 StyleSheets
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
