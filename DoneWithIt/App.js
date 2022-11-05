6ddc885d8721e2320d2ae65e301862c0bea478ed
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-04 20:47:59 -0600

    justifycontent

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -6,19 +6,22 @@ export default function App() {
     <View style={{
       backgroundColor: "#fff",
       flex: 1,
-      flexDirection: 'row'
+      flexDirection: 'row', //horizontal
+      justifyContent: 'center', //main axis
+      alignItems: 'center' //secondary axis
     }}
     >
       <View style={{
         backgroundColor: "dodgerblue",
         width: 100,
-        height: 100
+        height: 300,
+        alignSelf: 'flex-start'
       }}
       />
       <View style={{
         backgroundColor: "gold",
         width: 100,
-        height: 100
+        height: 200
       }}
       />
       <View style={{
